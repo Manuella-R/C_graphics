815a90bf7702aefd61fe9c4ebdd6b665f183ce54
Manuella-R <67600619+Manuella-R@users.noreply.github.com>
2024-09-05 16:03:29 +0300

    Add files via upload

---
+++ email.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import re
+
+
+df = pd.read_excel(r'TestFiles.xlsx')
+
+
+def generate_email(name):
+    clean_name = re.sub(r'[^a-zA-Z\s]', '', name)
+    parts = clean_name.split()
+    
+    if len(parts) == 1:
+        email = f"{parts[0].lower()}@gmail.com"
+    else:
+        email = f"{parts[0][0].lower()}{parts[-1].lower()}@gmail.com"
+    
+    return email
+
+df['Email'] = df['Student Name'].apply(generate_email)
+
+df['Email'] = df['Email'].apply(lambda x: re.sub(r'\s+', '', x))
+df['Email'] = df['Email'].apply(lambda x: x if pd.isna(df['Email'].where(df['Email'] == x).count()) else x[:-10] + str(df['Email'].where(df['Email'] == x).count()) + "@gmail.com")
+
+df.to_csv('C_graphics/students_with_emails.csv', index=False)
